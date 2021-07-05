e2b252660123184897afd6fd3971986b36f5533b
Trent Stollery <trent.stollery@millowgroup.com>
2021-07-05 17:16:51 +0930

    updates

--- acm.tf
+++ acm.tf
@@ -30,7 +30,7 @@ resource "aws_route53_record" "cert_validation" {
 }
 
 resource "aws_acm_certificate_validation" "cert" {
-  provider = "aws.${var.acm_provider_alias}"
+  provider = "${local.acm_provider}"
   for_each = aws_route53_record.cert_validation
   certificate_arn         = "${aws_acm_certificate.cert.arn}"
   validation_record_fqdns = ["${aws_route53_record.cert_validation[each.key].fqdn}"]
