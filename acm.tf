6d116b6780ccadbc00c1ed845e3f922c0ed1d1d1
Trent Stollery <trent.stollery@millowgroup.com>
2021-08-18 13:41:22 +0930

    updates to change vaildation

--- acm.tf
+++ acm.tf
@@ -15,28 +15,22 @@ resource "aws_acm_certificate" "cert" {
 }
 
 resource "aws_route53_record" "cert_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-      zone_id = "${var.route_53_primart_zone_id}"
-    }
-  }
-
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = each.value.zone_id
+  provider = aws.acm_provider
+  count = length(var.alt_domain_list) +1
+  zone_id = var.zone_id
+  name    = element(aws_acm_certificate.cert.domain_validation_options.*.resource_record_name, count.index)
+  type    = element(aws_acm_certificate.cert.domain_validation_options.*.resource_record_type, count.index)
+  records = [element(aws_acm_certificate.cert.domain_validation_options.*.resource_record_value, count.index)]
+  ttl     = 60
 }
 
 resource "aws_acm_certificate_validation" "cert" {
   provider = aws.acm_provider
-  for_each = aws_route53_record.cert_validation
-  certificate_arn         = "${aws_acm_certificate.cert.arn}"
-  validation_record_fqdns = ["${aws_route53_record.cert_validation[each.key].fqdn}"]
+
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = aws_route53_record.cert-validations.*.fqdn
 }
+
+
 
 
