3357240dd2f23ec79dd84c482dc80cd2788fe73d
Trent Stollery <trent.stollery@millowgroup.com>
2021-08-18 14:17:35 +0930

    revert

--- acm.tf
+++ acm.tf
@@ -9,28 +9,34 @@ provider "aws" {
 resource "aws_acm_certificate" "cert" {
   provider = aws.acm_provider
   domain_name               = "*.${var.hosted_zone}"
-  subject_alternative_names = var.alt_domain_list
+  subject_alternative_names = ["${var.hosted_zone}"]
   validation_method         = "DNS"
   tags                      = {}
 }
 
 resource "aws_route53_record" "cert_validation" {
-  provider = aws.acm_provider
-  count = length(var.alt_domain_list) +1
-  zone_id = var.route_53_primart_zone_id
-  name    = element(aws_acm_certificate.cert.domain_validation_options.*.resource_record_name, count.index)
-  type    = element(aws_acm_certificate.cert.domain_validation_options.*.resource_record_type, count.index)
-  records = [element(aws_acm_certificate.cert.domain_validation_options.*.resource_record_value, count.index)]
-  ttl     = 60
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = "${var.route_53_primart_zone_id}"
 }
 
 resource "aws_acm_certificate_validation" "cert" {
   provider = aws.acm_provider
-
-  certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = aws_route53_record.cert_validation.*.fqdn
+  for_each = aws_route53_record.cert_validation
+  certificate_arn         = "${aws_acm_certificate.cert.arn}"
+  validation_record_fqdns = ["${aws_route53_record.cert_validation[each.key].fqdn}"]
 }
-
 
 
 
