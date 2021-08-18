b8662beacc440608cb689172d8552f0b5a940955
Trent Stollery <trent.stollery@millowgroup.com>
2021-08-18 13:47:58 +0930

    updates

--- acm.tf
+++ acm.tf
@@ -28,7 +28,7 @@ resource "aws_acm_certificate_validation" "cert" {
   provider = aws.acm_provider
 
   certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = aws_route53_record.cert-validations.*.fqdn
+  validation_record_fqdns = aws_route53_record.cert_validation.*.fqdn
 }
 
 
